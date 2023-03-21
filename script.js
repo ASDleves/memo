74cb7f0bc464cd5ac3797482345679be3bd8767d
ASDleves <ricsi20020302@gmail.com>
2023-03-21 17:53:14 +0100

    játék ujraindit, kártyák eltünése kisebb bakival

--- script.js
+++ script.js
@@ -23,10 +23,15 @@ const LIST = [
   "kepek/kep5.jpg",
   "kepek/kep6.jpg",
 ];
+let lepesszam = 0;
 var kivalasztottak = [];
 const KIVALASZTOTTKEPEK = [];
+const KIVALASZTOTTKEPEK_MASODIK = [];
 let db = 0;
+let szamlalo = 0;
+let kepdb = 0
 $(function () {
+  létrehozás();
   //ide jön a kód
   /**1 .tedd ki a képeket a felső sectionbe
    * minden kép külön div-be kerüljön
@@ -39,24 +44,41 @@ $(function () {
   /**kisképekre kattintás */
   //fogd meg a kisképeket
   const FELSOKEPEK = $("#felso img");
-  //add hozzá az eseménykezelőt ,
   FELSOKEPEK.on("click", kepreKattintas);
 });
 
 function kepreKattintas() {
   const aktKep = event.target;
   //irasd ki a konzolra az aktuális elem src attributumát!
+  
   console.log(aktKep.id);
   console.log($(aktKep).attr("id"));
   /**Írjuk ki, hogy hányadik képre kattintottunK?  */
   /**kicseréljk a kép src */
   aktKep.src = LIST[aktKep.id];
   KIVALASZTOTTKEPEK.push($(aktKep).attr("id"));
-
+  KIVALASZTOTTKEPEK_MASODIK.push($(aktKep).attr("id"));
+  
   db++;
   if (db == 2) {
+    KIVALASZTOTTKEPEK_MASODIK[0] = LIST[KIVALASZTOTTKEPEK[0]]
+    KIVALASZTOTTKEPEK_MASODIK[1] = LIST[KIVALASZTOTTKEPEK[1]]
+    if (KIVALASZTOTTKEPEK_MASODIK[0] == KIVALASZTOTTKEPEK_MASODIK[1]){
+      console.log(KIVALASZTOTTKEPEK_MASODIK[0],KIVALASZTOTTKEPEK_MASODIK[1])
+      kepeltüntetes(KIVALASZTOTTKEPEK[0])
+      kepeltüntetes(KIVALASZTOTTKEPEK[1])
+      szamlalo++;
+    }
     db = 0;
-    visszafordit();
+  }else{
+  visszafordit();  
+  }
+  lepesszam++;
+  if (szamlalo == 6){
+    console.log("lépéseid száma",lepesszam/2)
+    lepeskiiras()
+    lepesszam = 0
+    szamlalo = 0
   }
 }
 
@@ -71,9 +93,11 @@ function visszafordit() {
     $(aktkep).attr("src", "kepek/hatter.jpg");
     aktkep = FELSOKEPEK.eq(KIVALASZTOTTKEPEK[1]);
     $(aktkep).attr("src", "kepek/hatter.jpg");
+    
     KIVALASZTOTTKEPEK.pop();
     KIVALASZTOTTKEPEK.pop();
   }, 1000);
+  kepdb = 0;
 }
 
 function osszeAllit() {
@@ -87,4 +111,61 @@ function osszeAllit() {
 }
 function keveres(array) {
   array.sort(() => Math.random() - 0.5);
+}
+function kepeltüntetes(id) {
+    var img = document.getElementById(id);
+    img.style.display = "none";
+}
+
+function lepeskiiras(){
+  var x = document.createElement("ARTICLE");
+  x.setAttribute("id", "myArticle");
+  document.body.appendChild(x);
+
+  var heading = document.createElement("H1");
+  var txt1 = document.createTextNode("Lépéseid");
+  heading.appendChild(txt1);
+  document.getElementById("myArticle").appendChild(heading);
+
+  var para = document.createElement("P");
+  var txt2 = document.createTextNode("Ennyi lépésből találtad meg az összes párt: "+lepesszam/2);
+  para.appendChild(txt2);
+  document.getElementById("myArticle").appendChild(para);
+
+}
+function létrehozás(){
+  const divElement = document.getElementById('jatek');
+
+const section1 = document.createElement('section');
+section1.setAttribute('id', 'felso');
+
+const section2 = document.createElement('section');
+section2.setAttribute('id', 'also');
+
+divElement.appendChild(section1);
+divElement.appendChild(section2);
+}
+function jatekkezdes(){
+  torles()
+  $(function () {
+    létrehozás();
+    //ide jön a kód
+    /**1 .tedd ki a képeket a felső sectionbe
+     * minden kép külön div-be kerüljön
+     */
+    keveres(LIST)
+    const FELSOELEM = $("#felso");
+    let tartalom = osszeAllit();
+    FELSOELEM.append(tartalom);
+  
+    /**kisképekre kattintás */
+    //fogd meg a kisképeket
+    const FELSOKEPEK = $("#felso img");
+    FELSOKEPEK.on("click", kepreKattintas);
+  });
+}
+function torles() {
+  const list = document.getElementById("jatek");
+  list.removeChild(list.firstElementChild);
+  list.removeChild(list.firstElementChild);
 }
